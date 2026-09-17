fbb480d47d258d4c1bac1bcdf32fe33a7735f88c
cpham2-commits <cpham2@berklee.edu>
2026-09-17 10:36:59 -0400

    update

--- pset2/frog_pond1.js
+++ pset2/frog_pond1.js
@@ -2,4 +2,4 @@ let numFrogs = prompt("How many frogs are in the pond?");
 let maxFrogs = 15;
 let isPondOvercrowded = numFrogs >= maxFrogs; // boolean
 let messageToPrint = isPondOvercrowded ? "Too crowded!" : "Hop in!";
-print(messageToPrint); 
+print(messageToPrint);
