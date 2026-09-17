66ea0743f39ad052e2b7a18ff27d8c2064f37c70
cpham2-commits <cpham2@berklee.edu>
2026-09-17 11:07:29 -0400

    upd

--- pset2/frog_pond2.js
+++ pset2/frog_pond2.js
@@ -4,5 +4,5 @@ let catchFlies = frogActivities[1];
 let tongueStretch = frogActivities[2];
 let swimingLessons = frogActivities[3]; 
 let whatFrogDoing = prompt("What is the frog doing? choose from 0-3");
-if (whatFrogDoing > 3) {print("Please choose a number between 0-3");}
+if (whatFrogDoing > 3) {print("(0) babysit tadpoles!")};
 print (frogActivities[whatFrogDoing]);
