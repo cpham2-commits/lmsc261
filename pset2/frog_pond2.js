7bc7f8629c992670630a80e9aeec5f56dc80fef4
cpham2-commits <cpham2@berklee.edu>
2026-09-17 10:31:51 -0400

    frog ponds

---
+++ pset2/frog_pond2.js
@@ -0,0 +1,6 @@
+let frogActivities = ["babysit tadpoles", "catch flies", "tongue stretch", "swiming lessons"];
+let babysitTadpoles = frogActivities[0];
+let catchFlies = frogActivities[1];
+let tongueStretch = frogActivities[2];
+let swimingLessons = frogActivities[3];
+let whatFrogDoing = prompt("What is the frog doing?");
