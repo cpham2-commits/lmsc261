a9a289e49e95d54c34ad1c5b48b9bac67dc0f58c
cpham2-commits <cpham2@berklee.edu>
2026-09-18 17:08:33 -0400

    update

--- pset2/frog_pond2.js
+++ pset2/frog_pond2.js
@@ -1,8 +1,8 @@
-let frogActivities = ["babysit tadpoles", "catch flies", "tongue stretch", "swiming lessons"];
-let babysitTadpoles = frogActivities[0];
-let catchFlies = frogActivities[1];
-let tongueStretch = frogActivities[2];
-let swimingLessons = frogActivities[3]; 
-let whatFrogDoing = prompt("What is the frog doing? choose from 0-3");
-if (whatFrogDoing > 3) {print("(0) babysit tadpoles!")};
-print (frogActivities[whatFrogDoing]);
+const activities = ["babysit tadpoles", 
+    "flies for lunch", 
+    "tongue stretch", 
+    "swimming lessons",];
+
+let index = prompt("what is the frog doing? choose from 0-3");
+index = index % 4;
+print(activities[index]);
